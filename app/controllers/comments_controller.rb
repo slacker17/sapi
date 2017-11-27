e5f301575e3bd5d56775dd4ea290191201e3931d
slacker17 <aguila1794@gmail.com>
2017-11-27 00:31:34 -0600

    Initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,54 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
+
+  # GET /comments
+  def index
+    @comments = Comment.all
+
+    render json: @comments
+  end
+
+  # GET /comments/1
+  def show
+    render json: @comment
+  end
+
+  # POST /comments
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /comments/1
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /comments/1
+  def destroy
+    @comment.destroy
+    render json: {"Message": "comment deleted"}
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: e.to_s }, status: :not_found
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def comment_params
+      params.permit(:body, :user_id)
+    end
+end
